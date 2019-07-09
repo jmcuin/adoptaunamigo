7cf7d46dbaf8acc1ebc2354ea06ce11c3ac99007
jmcuin <jmcuin@gmail.com>
2019-07-09 14:10:33 -0500

    Prueba con amigos 26

--- app/Http/Controllers/AmigoController.php
+++ app/Http/Controllers/AmigoController.php
@@ -95,6 +95,7 @@ class AmigoController extends Controller
             $request -> fotos[$i] -> storeAs('public/amigos', auth()->user()->id_rescatista.'_'.strtoupper($request -> nombre).'_'.$i.'.'.$archivos[$i] -> extension());
         }
         $amigo -> fotos = $fotos_amigo;
+        dd($amigo);
         $guardado = $amigo -> save();
 
         if($guardado)
