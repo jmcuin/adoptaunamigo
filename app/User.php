7d01c2967e3ecc4465a5379fe77dff98520498da
jmcuin <jmcuin@gmail.com>
2019-07-09 13:37:07 -0500

    Prueba con amigos 20

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     protected $primaryKey = 'id_user';
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email'
     ];
 
     /**
